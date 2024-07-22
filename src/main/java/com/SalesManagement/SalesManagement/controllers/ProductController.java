6fa508ba8139e0740841686a738743f42c534da1
Emanuel <emanuelestevaoofr@gmail.com>
2024-07-22 13:28:34 -0300

    FEAT: Complete CRUD for Products

--- src/main/java/com/SalesManagement/SalesManagement/controllers/ProductController.java
+++ src/main/java/com/SalesManagement/SalesManagement/controllers/ProductController.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,13 +28,37 @@ public class ProductController {
     @Autowired
     private final ProductService prodService;
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable) throws Exception {
         return ResponseEntity.ok(prodService.listAllProducts(pageable));
     }
 
-    @PostMapping()
-    public ResponseEntity<?> createProduct(@RequestBody @Valid ProductDTO product) throws Exception {
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) throws Exception {
+        Product product = prodService.findProductById(id);
+
+        if (product != null) {
+            return ResponseEntity.ok(prodService.findProductById(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductDTO product) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(prodService.saveNewProduct(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductDTO product)
+            throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(prodService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) throws Exception {
+        this.prodService.deleteProductById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/SalesManagement/SalesManagement/services/ProductService.java
+++ src/main/java/com/SalesManagement/SalesManagement/services/ProductService.java
@@ -10,7 +10,10 @@ import com.SalesManagement.SalesManagement.dto.ProductDTO;
 import com.SalesManagement.SalesManagement.entities.Product;
 import com.SalesManagement.SalesManagement.repositories.ProductRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +28,48 @@ public class ProductService {
         return prodRepository.findAll(pageable);
     }
 
+    @Transactional
     public Product saveNewProduct(ProductDTO productDTO) {
         Product newProduct = modelMapper.map(productDTO, Product.class);
 
         return prodRepository.save(newProduct);
+    }
+
+    public Product findProductById(Long id) {
+        if (id != null && id > 0)
+            return prodRepository.findById(id).orElse(null);
+        return null;
+
+    }
+
+    @Transactional
+    public Product updateProduct(Long id, ProductDTO productDTO) {
+        Product productUpdate = modelMapper.map(productDTO, Product.class);
+
+        if (id != null && id > 0) {
+            Optional<Product> productOptional = this.prodRepository.findById(id);
+
+            if (productOptional.isPresent()) {
+                Product product = productOptional.get();
+
+                product.setName(productUpdate.getName());
+
+                product.setDescription(productUpdate.getDescription());
+
+                product.setPrice(productUpdate.getPrice());
+
+                product.setStockQuantity(productUpdate.getStockQuantity());
+
+                return this.prodRepository.save(product);
+            }
+        }
+        return null;
+    }
+
+    @Transactional
+    public Product deleteProductById(Long id) {
+        if (id != null && id > 0)
+            this.prodRepository.deleteById(id);
+        return null;
     }
 }
